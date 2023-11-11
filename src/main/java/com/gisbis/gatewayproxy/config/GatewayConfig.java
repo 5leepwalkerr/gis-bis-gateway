d2fe1e261f4c948af7c6b026e06341e416d4bef7
awesomemax777 <cvb.cvb.2017.cvb@gmail.com>
2023-11-11 17:49:25 +0300

    init commit gateway

---
+++ src/main/java/com/gisbis/gatewayproxy/config/GatewayConfig.java
@@ -0,0 +1,25 @@
+package com.gisbis.gatewayproxy.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder locatorBuilder){
+      return locatorBuilder
+                .routes()
+                .route(data -> data
+                        .path("/render-data/**")
+                        .and()
+                        .method(HttpMethod.GET)
+                        .filters(filter -> filter
+                                .stripPrefix(1))
+                        .uri("lb://renderData"))
+                .build();
+    }
+}
